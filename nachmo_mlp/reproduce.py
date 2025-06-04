0216feabb86d62b0aed3bf6fbe91bd9e963db915
Andrey <74958459+Vlasenko2006@users.noreply.github.com>
2025-06-04 13:38:39 +0200

    Update reproduce.py

--- nachmo_mlp/reproduce.py
+++ nachmo_mlp/reproduce.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-
-"""Created on Mon Feb 27 13:36:38 2023.
+"""
+Created on Mon Feb 27 13:36:38 2023.
 
 @author: andreyvlasenko
 """
+
 import os
 import timeit
 
@@ -25,48 +25,36 @@ from utilities import load_data, load_data_reproduce
 
 from Ssurrogate import Ssurrogate
 
+##############################################
+# Set your path here
+##############################################
 
+Approach = "Naive"
 
-
-
-
-################################3###### Pankaj, set the pass here ################################3######
-
-#path_to_data = "/gpfs/work/vlasenko/NACHMO_data/concentrations_verwer_long2/"
-
-Naive = 0
-
-if Naive == 0:
-    path_to_model = "/gpfs/work/vlasenko/NACHMO_prog/Smatrix5/nachmo_coding/nachmo_mlp/Verwer_2h_nonshifted_proj/Reference_proj_CurCurr__torch.float32_Ssur__mean__tr_length_100_opt_3/version_1/"
-#    path_to_model = "/gpfs/work/vlasenko/NACHMO_prog/Smatrix5/nachmo_coding/nachmo_mlp/Verwer_2h_shifted_proj/Reference_proj_CurCurr__torch.float32_Ssur__mean__tr_length_100_opt_3/version_0/"
-    name_for_estimates = "Y_proj"
-elif Naive == 1:
-    path_to_model = "/gpfs/work/vlasenko/NACHMO_prog/Smatrix5/nachmo_coding/nachmo_mlp/Verwer_2h_nonshifted_proj/Reference_naive_CurCurr__torch.float32___mean__tr_length_100_opt_3/version_0/"
-#    path_to_model = "/gpfs/work/vlasenko/NACHMO_prog/Smatrix5/nachmo_coding/nachmo_mlp/Verwer_2h_shifted_proj/Reference_naive_CurCurr__torch.float32___mean__tr_length_100_opt_3/version_0/"
+if Approach == "Naive":
+    path_to_model = "/gpfs/work/NACHMO_prog/Naive/"
     name_for_estimates = "Y_naive"
+elif Approach == "Gated":
+    path_to_model = "/gpfs/work/NACHMO_prog/Gated/"
+    name_for_estimates = "Y_gated"
 else:
-    path_to_model = "/gpfs/work/vlasenko/NACHMO_prog/Smatrix5/nachmo_coding/nachmo_mlp/Verwer_2h_nonshifted_proj/Reference_naive_CurCurr__torch.float32___mean__tr_length_100_opt_3/version_0/"
-#    path_to_model = "/gpfs/work/vlasenko/NACHMO_prog/Smatrix5/nachmo_coding/nachmo_mlp/Verwer_2h_shifted_proj/Reference_naive_CurCurr__torch.float32___mean__tr_length_100_opt_3/version_0/"
-    name_for_estimates = "Y_naive2"
+    raise ValueError("Unknown Approach specified. Use 'Naive' or 'Gated'.")
 
-
-current_epoch = 2843  # set epoch
-ntimesteps = 220      # set the lenght of rollout
+current_epoch = 2843  # Set epoch
+ntimesteps = 220      # Set the length of rollout
 n_samples = 10
 
-################################3###### End of Pankaj, set the pass here ################################3######
-PATH = path_to_model + "model" + str(current_epoch) + ".pt"
+##############################################
+# End of path setup
+##############################################
 
+PATH = os.path.join(path_to_model, f"model{current_epoch}.pt")
 
-
-
-@hydra.main(version_base=None, config_path=path_to_model, config_name="hparams" )
+@hydra.main(version_base=None, config_path=path_to_model, config_name="hparams")
 def main(cfg: DictConfig):
-
     pl.seed_everything(2024)
 
-#   Loading configs   
-
+    # Load configs
     stepper_config = cfg.stepper_config
     net_config = cfg.net_config
     data_config = cfg.data_config
@@ -74,91 +62,86 @@ def main(cfg: DictConfig):
     data_dtype = eval(data_config.data_type)
     NN_dtype = eval(net_config.NN_dtype)
 
+    # Set computing device as CPU (no GPU so far)
+    net_config.device = "cpu"  # FIXME: Use GPU if available
 
-    if Naive ==2: 
-        #data_config.normalize_data = True
-        stepper_config.try_Ssur_matrix = True
-        stepper_config.learn_updates = False
-
- 
-
-
-#   Set computing device as cpu (no gpu so far)  
-
-    net_config.device = 'cpu'   #FIXME
-
-#   Selector for the type of c hemical scheme we want to compute. It just sets the number of reacting speceis 
-
+    # Select chemical scheme and set up stoichiometry matrix and rates
     if data_config.scheme == "Verwer":
         data_config.species = data_config.species_verwer
         cfg.data_config.species = data_config.species_verwer
         Smatrix = S_verwer
-        nrates=20
-         
-    if data_config.scheme == "OH":
+        nrates = 20
+    elif data_config.scheme == "OH":
         data_config.species = data_config.species_oh
         cfg.data_config.species = data_config.species_oh
         Smatrix = S_oh
         nrates = 4
+    else:
+        raise ValueError(f"Unknown chemical scheme: {data_config.scheme}")
 
-#   Set the amount of inputs and outputs in the NN
+    # Set number of inputs/outputs in the NN
     in_features = len(data_config["species"])
-    out_features = nrates if stepper_config["learn_rates"] else in_features  
+    out_features = nrates if stepper_config["learn_rates"] else in_features
 
+    # Load the data. ref is the reference data, max_c are the normalization coefficients
+    data, max_c = load_data(
+        cfg.data_config.data_path,
+        dtype=data_dtype,
+        normalize=data_config.normalize_data,
+        species=data_config["species"]
+    )
 
-#   Load the data, ref is the reference data, max_c are the normalization coefficients 
+    Ssur = Ssurrogate(
+        data,
+        max_c,
+        cut=cfg.extra_option,
+        dtype=data_dtype,
+        subtract_mean=data_config.subtarct_mean,
+        unit_S=data_config.unit_S,
+    )
 
-    data, max_c = load_data(cfg.data_config.data_path, dtype=data_dtype, normalize=data_config.normalize_data, species=data_config["species"])
-#    data, max_c = load_data(path_to_data, dtype=data_dtype, normalize=data_config.normalize_data, species=data_config["species"])
-    np.save('max_c', max_c)
-
-    data = data[:,:ntimesteps,:]
-
-
-    print('data.shape = ', data.shape)
-    Ssur = Ssurrogate(data, max_c, cut=cfg.extra_option, dtype=data_dtype, subtract_mean=data_config.subtarct_mean, unit_S=data_config.unit_S)
-
-    data = data[:n_samples,:,:]
-    print('data.shape = ', data.shape)
-#   Normalize Stoichiometry matrix 
+    # Normalize stoichiometry matrix
     for i in range(len(Smatrix)):
-        Smatrix[i, :] = Smatrix[i, :] / max_c[i] 
+        Smatrix[i, :] = Smatrix[i, :] / max_c[i]
 
+    # Prepare constants and Smatrix for estimates (future GPU use)
+    parameters_loader = constants_and_parameters_dataloader(
+        net_config.device,
+        Smatrix,
+        Ssur,
+        weights=np.ones(in_features),
+        dtype=data_dtype,
+    )
 
-#   Distributing some constants and Smatrix to devices for estimates on gpu (not needed so far, this is for future, when gpu is enabled)
-    parameters_loader = constants_and_parameters_dataloader(net_config.device, Smatrix, Ssur, weights = np.ones(in_features), dtype = data_dtype)
-
-#   Constructing the NN
+    # Construct the neural network
     net = MLP(in_features, out_features, **net_config)
     stepper = ChemicalTimeStepper(
-        net, device=net_config.device, dtype = NN_dtype, species_list=data_config["species"], parameters_loader = parameters_loader, **stepper_config
+        net,
+        device=net_config.device,
+        dtype=NN_dtype,
+        species_list=data_config["species"],
+        parameters_loader=parameters_loader,
+        **stepper_config,
     )
 
     model = RolloutModel(stepper, data_config["trajectory_length"], net_config.device)
-#    for var_name in stepper.state_dict():
-#        print(var_name, "\t", stepper.state_dict()[var_name])
 
+    # Instantiate (load weights and biases) the NN
+    model.load_state_dict(torch.load(PATH, map_location=torch.device("cpu")))
 
-#   Instantinate (load weights and biased) the NN
-    model.load_state_dict(torch.load(PATH, map_location=torch.device('cpu') ) )
-
-
-    # Compute your concentrations with a NN
-    y = no_grad_stepper(model.stepper, data, net_config.device, n_timepts = ntimesteps) # n_timepts is the ammount of timesteps to compute
+    # Compute your concentrations with NN
+    y = no_grad_stepper(
+        model.stepper, data, net_config.device, n_timepts=ntimesteps
+    )  # n_timepts is the number of timesteps to compute
 
     y = np.asarray(y)
     data = np.asarray(data)
 
-    print("data.shape", data.shape, "y.shape", y.shape)  
+    print("data.shape", data.shape, "y.shape", y.shape)
 
-    for i in range(0,len(data_config.species)):
-        print("max. data[:,:,i]", np.max(data[:,:,i]), "y.max", np.max(y[:,:,i]) )
-
-#   Save your results
+    # Save results
     np.save(name_for_estimates, y)
-    np.save("Y_ref" , data)
-
-
+    np.save("Reference", data)
 
 if __name__ == "__main__":
     main()
